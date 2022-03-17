8a0c1723f8bf844330a1393f3d0e646d93768f29
Elijah Koulaxis <90087463+kx0101@users.noreply.github.com>
2022-03-17 22:46:41 +0200

    Update main.java

--- main.java
+++ main.java
@@ -2,9 +2,7 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
         int[] result = new int[m+n];
-        int i = 0;
-        int j = 0;
-        int k = 0;
+        int i, j, k = 0;
         
         while(i!=m && j!=n){
             if(nums1[i]>nums2[j]){
